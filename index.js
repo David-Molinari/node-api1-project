43beaae76aa08e7becd9eb4dd1c841c2f87da648
David Molinari <Dmolina5@Davids-MBP-2.hsd1.pa.comcast.net>
2020-04-09 13:12:30 -0400

    Finished project

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const shortid = require('shortid');
+
+const server = express();
+
+server.use(express.json());
+
+let users = [
+    {
+        name: 'John Doe',
+        bio: 'random things',
+        id: shortid.generate()
+    }
+]
+
+server.get('/', (req, res) => {
+  res.json({ api: 'running.....'});
+});
+
+server.post('/api/users', (req, res) => {
+    if (req.body.name !== undefined){
+        const userInfo = req.body;
+        userInfo.id = shortid.generate();
+        users.push(userInfo);
+        res.status(201).json(userInfo);
+    } else if (req.body === undefined || req.body === undefined) {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    } else {
+        res.status(500).json({errorMessage: "There was an error while saving the user to the database"})
+    }
+});
+
+server.get('/api/users', (req, res) => {
+    if (req.body !== undefined){
+        res.status(200).json(users);
+    } else {
+        res.status(500).json({errorMessage: "There was an error while getting the users"})
+    }
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    const user = users.find((user) => user.id == id)
+
+    if(user) {
+        res.status(200).json(user)
+    } else if (user.name == undefined){
+        res.status(404).json({message: "The user with the specified ID does not exist"})
+    } else {
+        res.status(500).json({message: "The user information could not be retrieved."})
+    }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+
+    res.status(200).json({message: `Deleted the user with id: ${id}`}
+    )
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const user = users.find(u => u.id == req.params.id);
+  
+    if (!user) {
+      res.status(404).json({ message: 'User does not exist' });
+    } else {
+      Object.assign(user, req.body);
+  
+      res.status(200).json(user);
+    }
+  });
+
+
+const port = 5000;
+server.listen(port, () => console.log(`\n== api on ${port} ==\n`))
